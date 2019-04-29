fd61296265a9d96880029d14dd1fe161a9b03272
Boulix <maxenceperso@gmail.com>
2019-04-29 15:14:26 +0200

    Tiens les getters !

---
+++ src/robots/RobotLent.java
@@ -0,0 +1,17 @@
+package robots;
+
+public class RobotLent extends Robot {
+
+	private final int COEFVITESSE = 1;
+	
+	public RobotLent(int X, int Y, int idRobot) {
+		super(X, Y, idRobot);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCOEFVITESSE() {
+		return COEFVITESSE;
+	}
+	
+	
+}
---
+++ src/robots/RobotRapide.java
@@ -0,0 +1,18 @@
+package robots;
+
+public class RobotRapide extends Robot {
+
+	private final int COEFVITESSE = 3;
+	
+	public RobotRapide(int X, int Y, int idRobot) {
+		super(X, Y, idRobot);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCOEFVITESSE() {
+		return COEFVITESSE;
+	}
+	
+	
+
+}
